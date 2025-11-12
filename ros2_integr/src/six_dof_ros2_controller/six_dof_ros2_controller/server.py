f1c38712ec27201b13026b4a7b24a626bd70695c
Mohammadreza Anvari <mrezanvari@gmail.com>
2025-11-12 18:39:21 -0500

    Update server.py

--- ros2_integr/src/six_dof_ros2_controller/six_dof_ros2_controller/server.py
+++ ros2_integr/src/six_dof_ros2_controller/six_dof_ros2_controller/server.py
@@ -124,7 +124,7 @@ class SetPoseServer(Node):
 
 
 def init_robot(serial_conn: ThreadedSerial):
-    serial_conn.entry_point.send("set gain 8\r\n")
+    serial_conn.entry_point.send("set gain 6\r\n")
     time.sleep(1)
     serial_conn.entry_point.send("jacobi\r\n")
     time.sleep(1)
