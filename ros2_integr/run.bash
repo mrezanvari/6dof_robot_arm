7d2cb2617df12463a4ff36a85bf82f21f5257bcb
Mohammadreza Anvari <mrezanvari@gmail.com>
2026-03-23 19:16:31 -0400

    Update run.bash for ros

--- ros2_integr/run.bash
+++ ros2_integr/run.bash
@@ -2,4 +2,8 @@ clear
 colcon build
 source install/setup.bash
 chmod -R 777 .
-ros2 run six_dof_ros2_controller $@
+if [ $1 == "sim" ]; then
+	ros2 launch AssemblyURDF_description $2.launch.py
+else
+	ros2 run six_dof_ros2_controller $@
+fi
